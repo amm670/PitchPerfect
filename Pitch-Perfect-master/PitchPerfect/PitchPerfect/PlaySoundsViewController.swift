4813f7990b8349302f2afe989f855cc5342fbe43
Alexandra Montgomery <amm670@scarletmail.rutgers.edu>
2020-08-25 17:18:24 -0400

    Add files via upload

---
+++ Pitch-Perfect-master/PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Alexandra Montgomery on 08/01/2019.
+//  Copyright © 2020 Alexandra Montgomery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class PlaySoundsViewController: UIViewController  {
+    // /////9
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    /////////10
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+///////9
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        ////13
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+///////14
+stopAudio()
+        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+////////11
+        setupAudio()
+        
+    }
+   //// ////12
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pitch-Perfect-master/README.md
@@ -0,0 +1,35 @@
+# Pitch-Perfect
+ 
+
+![Pitch Perfect](https://github.com/Abdu11a/Pitch-Perfect/blob/master/PitchPerfect/PitchPerfect%20Screen/Screen%20Shot%201441-06-18%20at%205.14.29%20PM.png)
+
+## Overview
+iPhone app that will allow users to record their voice and will then modulate the recorded audio to sound like a Chipmunk or Darth Vader. This app will also let the user speed up or slow down the rate of playback, and experience fun echo and reverb effects.
+
+---
+---
+
+## the app have tow view controller :
+
+- **The First View**  :  allow users to record their voice and will then modulate the recorded audio.
+
+<img src="https://github.com/Abdu11a/Pitch-Perfect/blob/master/PitchPerfect/PitchPerfect%20Screen/Screen%201.png" width=400>
+
+
+
+- **The Second View**  :  allow users to play their recorded voice to sound like a Chipmunk or Darth Vader and also let the user speed up or slow down the rate of playback, and experience fun echo and reverb effects.
+ 
+
+<img src="https://github.com/Abdu11a/Pitch-Perfect/blob/master/PitchPerfect/PitchPerfect%20Screen/Screen%202.png" width=400>
+
+
+---
+## Requirements to run this app
+
+**To run this app you need to [Downlaod the PitchPerfect](https://github.com/Abdu11a/Pitch-Perfect/archive/master.zip).**
+
+### Versions:
+
+- [![Xcode Version](https://img.shields.io/badge/Xcode-10+-success.svg)](https://swift.org) 
+- [![Swift Version](https://img.shields.io/badge/Swift-4+-success.svg)](https://swift.org)
+- [![Platform](https://img.shields.io/cocoapods/p/LFAlertController.svg?style=flat)](https://swift.org)
